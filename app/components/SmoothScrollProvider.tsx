4985a43d02b1f49900797696f9002d3653537683
bloodstormm_ <nicolasmalaquias2015@hotmail.com>
2026-06-10 16:31:41 -0300

    Lenis update

--- app/components/SmoothScrollProvider.tsx
+++ app/components/SmoothScrollProvider.tsx
@@ -15,9 +15,17 @@ export function SmoothScrollProvider({ children }: { children: React.ReactNode }
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
+      syncTouch: false,
+      syncTouchLerp: 0.1,
+      touchInertiaExponent: 35,
+      touchMultiplier: 1,
+      wheelMultiplier: 1,
+      gestureOrientation: 'vertical',
+      orientation: 'vertical',
+      infinite: false,
     });
 
     lenisRef.current = lenis;
